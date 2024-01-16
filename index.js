1a498465d2c821751c05cc1861444fafe8d39d8d
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-01-17 00:20:08 +0530

    Edit & Update Route

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.set("views", path.join(__dirname,"views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
 
 main().then(()=>{
     console.log("Connection Successful");
@@ -43,6 +46,22 @@ app.post("/chats",(req,res)=>{
     .catch((err)=>{
         console.log(err);
     });
+    res.redirect("/chats");
+});
+
+//Edit Route
+app.get("/chats/:id/edit/",async (req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat});
+});
+
+//Update Route
+app.put("/chats/:id",async (req,res)=>{
+    let {id} = req.params;
+    let {msg : newmsg} = req.body;
+    let updatechat = await Chat.findByIdAndUpdate(id, {msg : newmsg}, {runValidators:true}, {new:true});
+    console.log("Updated");
     res.redirect("/chats");
 });
 
